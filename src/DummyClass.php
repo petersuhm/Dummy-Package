37f35bee9719fb3c49c585370505fe8cf1b8cfe8
Peter Suhm <peter@suhm.dk>
2015-06-18 20:48:42 +0200

    Breaking change

--- src/DummyClass.php
+++ src/DummyClass.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * @version 1.0.0
+ * @version 2.0.0
  */
 
 namespace PeterSuhm;
 
 class DummyClass
 {
-    public function sayHello()
+    public function sayHello($name)
     {
-        echo 'Hello!';
+        echo "Hello, {$name}!";
     }
 }
